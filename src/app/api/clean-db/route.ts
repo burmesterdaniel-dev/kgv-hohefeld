2f448332736e3d746eea131c8224fe1c0397ab21
Daniel Burmester <daniel@burmester.dev>
2026-04-03 15:37:09 +0200

    fix: db clean query column names

--- src/app/api/clean-db/route.ts
+++ src/app/api/clean-db/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
       db.execute('DELETE FROM events WHERE id NOT IN (SELECT MIN(id) FROM events GROUP BY title, date_string)'),
       db.execute('DELETE FROM gardens WHERE id NOT IN (SELECT MIN(id) FROM gardens GROUP BY title, area)'),
       db.execute('DELETE FROM photos WHERE id NOT IN (SELECT MIN(id) FROM photos GROUP BY filepath)'),
-      db.execute('DELETE FROM guestbook WHERE id NOT IN (SELECT MIN(id) FROM guestbook GROUP BY author, text)')
+      db.execute('DELETE FROM guestbook WHERE id NOT IN (SELECT MIN(id) FROM guestbook GROUP BY name, message)')
     ];
 
     await Promise.all(tablePromises);
